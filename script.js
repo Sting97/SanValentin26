46f484d9e2f09cd5412b512b1e6b22808161349f
Sting97 <campana-daniel@hotmail.com>
2026-02-14 00:40:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-const startDate = new Date("2024-02-14T00:00:00");
 
-function updateTime() {
-  const now = new Date();
-  const diff = now - startDate;
-
-  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
-  const minutes = Math.floor((diff / (1000 * 60)) % 60);
-  const seconds = Math.floor((diff / 1000) % 60);
-
-  document.getElementById("time").innerText =
-    `${days} días ${hours} horas ${minutes} minutos ${seconds} segundos`;
-}
-
-setInterval(updateTime, 1000);
-updateTime();
